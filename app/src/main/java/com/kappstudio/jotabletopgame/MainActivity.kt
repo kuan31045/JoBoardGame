66bcbb01f2b7699b0bf43157c9d43713e13a8ac4
AKuan <kuan31045@gmail.com>
2021-10-19 02:42:40 +0800

    HomePage UI + ToolsPage UI
    •Bottom navigation
    •ToolBar
    •HomePage UI
    •ToolsPage UI

--- app/src/main/java/com/kappstudio/jotabletopgame/MainActivity.kt
+++ app/src/main/java/com/kappstudio/jotabletopgame/MainActivity.kt
@@ -2,10 +2,49 @@ package com.kappstudio.jotabletopgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.kappstudio.jotabletopgame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+
+        setSupportActionBar(binding.toolbar)
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment, R.id.gameFragment, R.id.toolFragment
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.ivLogo.visibility = View.GONE
+
+            binding.tvTitle.text = when (destination.id) {
+
+                R.id.homeFragment -> {
+                    binding.ivLogo.visibility = View.VISIBLE
+                    ""
+                }
+                R.id.gameFragment -> getString(R.string.game_page)
+                R.id.toolFragment -> getString(R.string.tool_page)
+
+                else -> ""
+            }
+
+        }
     }
 }
