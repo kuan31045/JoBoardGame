8cff783a3ec77ba564c7c526d7f80ab0a9e00c51
AKuan <kuan31045@gmail.com>
2023-10-08 18:17:49 +0800

    Fix  bottom navigation view bug

--- app/src/main/java/com/kappstudio/joboardgame/MainActivity.kt
+++ app/src/main/java/com/kappstudio/joboardgame/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.libraries.places.api.Places
@@ -75,7 +76,6 @@ class MainActivity : AppCompatActivity() {
         navController = findNavController(R.id.nav_host_fragment_activity_main)
         binding.toolbar.setNavigationOnClickListener {
             navController.popBackStack()
-
         }
 
         setNavController()
@@ -93,6 +93,11 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.navView.setupWithNavController(navController)
+        binding.navView.setOnItemSelectedListener {
+            NavigationUI.onNavDestinationSelected(it, navController)
+            navController.popBackStack(it.itemId, inclusive = false)
+            true
+        }
     }
 
     private fun setBarAttr() {
